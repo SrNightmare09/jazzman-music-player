593b3a57dddc9270f311c1de317378389a814516
SrNightmare09 <srnightmare09@gmail.com>
2023-07-21 22:44:21 +0530

    chore: start dev for integration of frontend and backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,37 +1,36 @@
 // DO NOT REMOVE
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::io;
-
-mod database;
-mod fetch_files;
-
-use database::db;
-use fetch_files::file_scanning::get_data;
-
-fn main() -> Result<(), io::Error> {
-
-    let path: &str = "D:/Music/";
-
-    db::create_table();
-
-    match get_data(path) {
-        Ok(songs) => {
-            for song in &songs {
-                db::insert_item(song);
-            }
-        },
-        Err(err) => println!("{}", err)
-    }
-
-    Ok(())
-}
-
-/*
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![fetch_files::get_folders])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-*/
+
+// use std::io;
+
+// mod database;
+// mod fetch_files;
+
+// use database::db;
+// use fetch_files::file_scanning::get_data;
+
+// fn main() -> Result<(), io::Error> {
+
+//     let path: &str = "D:/Music/";
+
+//     db::create_table();
+
+//     match get_data(path) {
+//         Ok(songs) => {
+//             for song in &songs {
+//                 db::insert_item(song);
+//             }
+//         },
+//         Err(err) => println!("{}", err)
+//     }
+
+//     Ok(())
+// }
+
