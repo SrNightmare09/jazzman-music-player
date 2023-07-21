cfca10811e75938a5cfa45a02f74f6c06b7aeeb6
SrNightmare09 <srnightmare09@gmail.com>
2023-07-21 22:44:56 +0530

    chore: initiate tauri commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,30 +7,3 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
-// use std::io;
-
-// mod database;
-// mod fetch_files;
-
-// use database::db;
-// use fetch_files::file_scanning::get_data;
-
-// fn main() -> Result<(), io::Error> {
-
-//     let path: &str = "D:/Music/";
-
-//     db::create_table();
-
-//     match get_data(path) {
-//         Ok(songs) => {
-//             for song in &songs {
-//                 db::insert_item(song);
-//             }
-//         },
-//         Err(err) => println!("{}", err)
-//     }
-
-//     Ok(())
-// }
-
