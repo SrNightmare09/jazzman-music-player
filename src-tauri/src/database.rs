ffb6996a7f33b4dc2a5dcd5ff9b2fdfdaaa5ba58
SrNightmare09 <srnightmare09@gmail.com>
2023-07-21 13:22:41 +0530

    chore: remove unnecessary functions

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,40 +1,29 @@
-#![allow(unused)]
-
 pub mod db {
 
     use rusqlite::{Connection, Result};
 
-    pub fn initialize() -> Result<()> {
+    use crate::fetch_files::Song;
+
+    pub fn create_table() -> Result<()> {
+        let conn = open_connection()?;
+
+        conn.execute(
+            "
+                CREATE TABLE songs (
+                    song_name TEXT,
+                    song_artist TEXT,
+                    song_album TEXT,
+                    song_length INTEGER
+                );
+            ",
+            [],
+        )?;
+
+        Ok(())
+    }
+
+    fn open_connection() -> Result<Connection> {
         let path: &str = "../db/library.db";
-        let conn = open_connection(path)?;
-
-        create_tables(&conn)?;
-
-        Ok(())
-    }
-
-    fn open_connection(path: &str) -> Result<Connection> {
         Connection::open(path)
-    }
-
-    fn create_tables(conn: &Connection) -> Result<()> {
-        conn.execute(
-            "create table if not exists cat_complex (
-                id integer primary key,
-                name text not null unique
-            )",
-            [],
-        )?;
-
-        conn.execute(
-            "create table if not exists cats (
-                id integer primary key,
-                name text not null unique,
-                color_id integer not null references cat_complex(id)
-            )",
-            [],
-        )?;
-
-        Ok(())
     }
 }
