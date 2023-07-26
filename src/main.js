d9a50c61e90cee86c35b977e187b3635528a48c5
SrNightmare09 <srnightmare09@gmail.com>
2023-07-26 16:11:22 +0530

    feat: append span child with background image for loading

--- src/main.js
+++ src/main.js
@@ -4,3 +4,15 @@ function toggleSubOptions(id) {
       subOptions.style.display = subOptions.style.display === "none" ? "block" : "none";
   }
 }
+
+function getLibraryArtwork() {
+  let a =[4, 2, 3, 1, 23, 12, 3];
+
+  const main_view = document.getElementById('test');
+
+  let child = document.createElement('span');
+  child.classList.add('grid-cell');
+  child.style.backgroundImage = 'url(assets/Cover.jpg)';
+
+  main_view.appendChild(child);
+}
