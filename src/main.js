22af69dfda0bcffdeb6bc68820e6d6feaa75bd5d
SrNightmare09 <srnightmare09@gmail.com>
2023-07-28 15:58:49 +0530

    feat: objects returned are parsed as json and returned to console for testing

--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
 function toggleSubOptions(id) {
-  const subOptions = document.getElementById('playlistsOptions');
-  if (id === 'playlists') {
-      subOptions.style.display = subOptions.style.display === "none" ? "block" : "none";
-  }
+	const subOptions = document.getElementById('playlistsOptions');
+	if (id === 'playlists') {
+		subOptions.style.display = subOptions.style.display === "none" ? "block" : "none";
+	}
 }
 
 function getLibraryArtwork() {
-  let a =[4, 2, 3, 1, 23, 12, 3];
+	const main_view = document.getElementById('test');
 
-  const main_view = document.getElementById('test');
+	let child = document.createElement('span');
+	child.classList.add('grid-cell');
+	child.style.backgroundImage = 'url(assets/Cover.jpg)';
 
-  let child = document.createElement('span');
-  child.classList.add('grid-cell');
-  child.style.backgroundImage = 'url(assets/Cover.jpg)';
+	main_view.appendChild(child);
+}
 
-  main_view.appendChild(child);
+async function rustfun() {
+	try {
+		const result = await window.__TAURI__.invoke('fetch_item', {});
+		let albums = [];
+		for (let i = 0; i < result.length(); i++) {
+			let info = {
+				name: result[i].name,
+				artwork: result[i].artwork
+			};
+			albums.push(info);
+		}
+	} catch (error) {
+		console.error('Error calling Rust function:', error);
+	}
 }
