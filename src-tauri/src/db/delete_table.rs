bf824fccf8afac0f050e13b4cfb74bc6249897e5
SrNightmare09 <srnightmare09@gmail.com>
2023-07-29 15:32:02 +0530

    feat: add function to delete table

--- src-tauri/src/db/delete_table.rs
+++ src-tauri/src/db/delete_table.rs
@@ -1,23 +1,15 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::Result;
 
 use super::open_connection::open_connection;
 
-pub fn delete_all_data_in_table() -> Result<()> {
-    // Open a connection to the database
-    ;
+pub fn delete_table() -> Result<()> {
     let conn = open_connection()?;
 
-    // Specify the name of the table from which you want to delete all data
     let table_name = "songs";
 
-    // SQL query to delete all data from the table
-    let sql = format!("DELETE FROM {}", table_name);
+    let sql = format!("DROP TABLE IF EXISTS {}", table_name);
 
-    // Execute the query
-    conn.execute(&sql, params![])?;
-
-    // All data in the table is now deleted
-    println!("All data in table {} has been deleted.", table_name);
+    conn.execute(&sql, [])?;
 
     Ok(())
 }
