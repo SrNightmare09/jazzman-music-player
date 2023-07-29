0074d3c57549b4d9fdc5a1919e07ddb0a1e03051
SrNightmare09 <srnightmare09@gmail.com>
2023-07-29 14:24:21 +0530

    feat: add function to delete table

---
+++ src-tauri/src/db/delete_table.rs
@@ -0,0 +1,16 @@
+use rusqlite::Result;
+
+use super::open_connection::open_connection;
+
+pub fn delete_table() -> Result<()> {
+
+    let conn = open_connection()?;
+
+    let table_name = "songs";
+
+    let sql = format!("DROP TABLE IF EXISTS {}", table_name);
+
+    conn.execute(&sql, [])?;
+
+    Ok(())
+}
