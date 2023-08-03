3f34e7a4d6b0ab06372eb76879e03a6256b86ea7
SrNightmare09 <srnightmare09@gmail.com>
2023-08-03 21:02:07 +0530

    feat: can now pass in dynamic vecs for table init

--- src-tauri/src/controller/initialize.rs
+++ src-tauri/src/controller/initialize.rs
@@ -4,6 +4,13 @@ use super::super::db::create_table::create_table;
 
 #[tauri::command]
 pub fn initialize() {
-    create_table("songs");
-    create_table("playlists");
+
+    let songs_columns: Vec<&str> = vec!["song_name", "song_artist", "song_album", "song_artwork", "song_length", "song_id"];
+    if let Err(err) = create_table("songs", songs_columns) {
+        eprintln!("Error creating table: {}", err.to_string());
+    } else {
+        ()
+    }
+
+    let playlist_columns: Vec<&str> = vec!["playlist_name", "playlist_sngs"];
 }
--- src-tauri/src/db/create_table.rs
+++ src-tauri/src/db/create_table.rs
@@ -2,12 +2,48 @@ use rusqlite::Result;
 
 use super::open_connection::open_connection;
 
-pub fn create_table(table_name: &str) -> Result<()> {
+pub fn create_table(table_name: &str, columns: Vec<&str>) -> Result<()> {
     let conn = open_connection()?;
 
-    let sql = format!("CREATE TABLE {} (song_name TEXT, song_artist TEXT, song_album TEXT, song_artwork TEXT, song_length INTEGER, song_id TEXT);", table_name);
+    let fields: String = {
+        let mut statement = String::new();
+        for column in &columns {
+            let field = format!("{} {}, ", column, get_data_type(column));
+            statement.push_str(&field);
+        }
+        statement.pop();
+        statement.pop(); // remove comma and space from end
+        statement
+    };
+
+    let sql = format!("CREATE TABLE {} ({});", table_name, &fields);
 
     conn.execute(&sql,[],)?;
 
     Ok(())
+}
+
+fn get_data_type(column: &str) -> String {
+    let text = vec!["song_name", "song_artist", "song_album", "song_artwork"];
+    let integer = vec!["song_length"];
+    let var_char = vec!["song_id"];
+
+    for item in &text {
+        if &column == item {
+            return String::from("TEXT");
+        }
+    }
+
+    for item in &integer {
+        if &column == item {
+            return String::from("INTEGER");
+        }
+    }
+
+    for item in &var_char {
+        if &column == item {
+            return String::from("VARCHAR(10)");
+        }
+    }
+    return String::from("UNKNOWN");
 }
