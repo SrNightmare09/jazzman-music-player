5405d5bf152f1e2edea244e885db9f0130133ac4
SrNightmare09 <srnightmare09@gmail.com>
2023-08-06 02:14:24 +0530

    feat(wip): view songs of albums upon clicking them in sidebar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,8 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             controller::scan_music::scan_music,
             db::fetch_item::fetch_item,
-            db::fetch_specific::fetch_specific,
+            db::fetch_specific_artwork::fetch_specific_artwork,
+            db::fetch_one_to_one::fetch_one_to_one,
             controller::initialize::initialize
         ])
         .run(tauri::generate_context!())
--- src/main.js
+++ src/main.js
@@ -69,6 +69,7 @@ async function getAlbums() {
 	album.forEach((album_artist) => {
 		let child = document.createElement('div');
 		child.classList.add('sidebar-item');
+		child.setAttribute("id", "sidebar-album");
 		child.innerText = album_artist;
 		album_list.appendChild(child);
 	});
@@ -82,8 +83,64 @@ document.addEventListener('click', (e) => {
 	}
 });
 
+document.addEventListener('click', (e) => {
+	const album_sidebar = e.target.closest("#sidebar-album");
+
+	if (album_sidebar) {
+		showAlbumDetails(album_sidebar.innerText);
+	}
+});
+
+async function showAlbumDetails(album) { // hell(p)
+
+	const scroll_view = document.getElementById('main-view-scroll');
+	const album_details = document.getElementById('album-details');
+	const album_name = document.getElementById('album-details-name');
+	const artist_name = document.getElementById('album-details-artist');
+	const album_artwork = document.getElementById('album-details-artwork');
+	const song_list = document.getElementById('album-details-songs');
+
+	const songs = await tauri.invoke('fetch_one_to_one', { selectQry: 'song_name', whereQry: 'song_album', query: album });
+	const artist = [...new Set(await tauri.invoke('fetch_one_to_one', { selectQry: 'song_artist', whereQry: 'song_album', query: album }))];
+	const artwork = [...new Set(await tauri.invoke('fetch_one_to_one', { selectQry: 'song_artwork', whereQry: 'song_album', query: album }))];
+
+	scroll_view.style.display = 'none';
+	album_details.style.display = 'block';
+	album_name.innerText = album;
+	artist_name.innerText = artist[0];
+	album_artwork.style.backgroundImage = `url('${artwork}')`;
+	song_list.innerHTML = '';
+
+	let e_song_wrapper = document.createElement('div');
+	e_song_wrapper.setAttribute('id', 'album-details-album-song');
+	song_list.appendChild(e_song_wrapper);
+
+	let song_wrapper = document.getElementById('album-details-album-song');
+	let song_s_no = document.createElement('div');
+	song_s_no.setAttribute('id', 'song-s-no');
+	song_s_no.innerText = '69';
+
+	let song_name = document.createElement('div');
+	song_name.setAttribute('id', 'song-name');
+	song_name.innerText = songs[0];
+
+	let song_like_status = document.createElement('div');
+	song_like_status.setAttribute('id', 'song-like-status');
+	song_like_status.innerText = 'L';
+
+	let song_length = document.createElement('div');
+	song_length.setAttribute('id', 'song-length');
+	song_length.innerText = '0:00';
+
+	song_wrapper.appendChild(song_s_no);
+	song_wrapper.appendChild(song_name);
+	song_wrapper.appendChild(song_like_status);
+	song_wrapper.appendChild(song_length);
+
+}
+
 async function fetchArtistAlbums(artist) {
-	const albums = await tauri.invoke('fetch_specific', { query: artist });
+	const albums = await tauri.invoke('fetch_specific_artwork', { query: artist });
 	const main_view = document.getElementById('main-view');
 
 	main_view.innerHTML = '';
