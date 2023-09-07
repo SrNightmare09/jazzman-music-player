51188d41d9aaac4b2c2bdd68882f04d18f94bbfa
SrNightmare09 <srnightmare09@gmail.com>
2023-09-07 23:32:27 +0530

    refactor: format the fields of the tables

--- src-tauri/src/controller/initialize.rs
+++ src-tauri/src/controller/initialize.rs
@@ -5,14 +5,27 @@ use super::super::db::create_table::create_table;
 #[tauri::command]
 pub fn initialize() {
 
-    let songs_columns: Vec<&str> = vec!["song_name", "song_artist", "song_album", "song_artwork", "song_length", "song_id"];
+    let songs_columns: Vec<&str> = vec![
+        "song_name",
+        "song_artist",
+        "song_album",
+        "song_artwork",
+        "song_length",
+        "song_id"
+    ];
+
     if let Err(err) = create_table("songs", songs_columns) {
         eprintln!("Error creating table: {}", err.to_string());
     } else {
         ()
     }
 
-    let playlist_columns: Vec<&str> = vec!["playlist_name", "playlist_id", "playlist_songs"]; // songs is an array with song ids
+    let playlist_columns: Vec<&str> = vec![
+        "playlist_name",
+        "playlist_id",
+        "playlist_songs"
+    ]; // songs is an array with song ids
+
     if let Err(err) = create_table("playlists", playlist_columns) {
         eprintln!("Error creating table: {}", err.to_string());
     } else {
